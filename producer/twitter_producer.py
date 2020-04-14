1557f23d7950146096d01d5725654d714075c1b6
Kaushiksekar <kaushik3993@gmail.com>
2020-04-15 01:24:31 +0530

    Changed filter text from trump to covid

--- producer/twitter_producer.py
+++ producer/twitter_producer.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	stream = Stream(auth, out)
-	stream.filter(track="trump")
+	stream.filter(track="covid")
