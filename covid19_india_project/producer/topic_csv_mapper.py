b2efae198ae89efa13d41749a9f4f7e1d5208031
Kaushiksekar <kaushik3993@gmail.com>
2020-04-16 00:08:59 +0530

    Commit for adding resources folder in gitignore and added mapper dict for csv and topics

---
+++ covid19_india_project/producer/topic_csv_mapper.py
@@ -0,0 +1,10 @@
+TOPIC_CSV_MAPPER = {
+	"IndividualDetails.csv": "individual-details",
+	"covid_19_india.csv": "state-wise-history",
+	"HospitalBedsIndia.csv": "hospital-beds",
+	"ICMRTestingLabs.csv": "icmr-testing-labs",
+	"population_india_census2011.csv": "population-india-census-twenty-eleven",
+	"AgeGroupDetails.csv": "age-group-details",
+	"StatewiseTestingDetails.csv": "state-wise-testing-history",
+	"ICMRTestingDetails.csv": "icmr-testing-detils"
+}
